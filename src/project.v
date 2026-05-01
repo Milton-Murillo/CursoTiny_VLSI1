344c563d1cf4f02eac946f0fd79b7e6b2a3a1f94
Milton-Murillo <141180204+Milton-Murillo@users.noreply.github.com>
2026-04-30 23:04:33 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_example (
@@ -10,28 +5,41 @@ module tt_um_example (
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    output wire [7:0] uio_oe,   // IOs: Enable path
+    input  wire       ena,      // Always 1 when powered
+    input  wire       clk,      // Clock
+    input  wire       rst_n     // Active-low reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out[7:1]  = 0;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out[7:1] = 0;
-  assign uio_oe  = 0;
-  
+    // Señales internas
+    wire        reg_load_i;
+    wire        lfsr_enable_i;
+    wire [7:0]  reg_data_o;
+    wire        lfsr_done_o;
 
-  // List all unused inputs to prevent warnings
+    // Asignación de entradas
+    assign reg_load_i     = ui_in[0];
+    assign lfsr_enable_i  = ena;
+
+    // Asignación de salidas
+    assign uo_out = reg_data_o;
+
+    assign uio_out[0]   = lfsr_done_o;
+    assign uio_out[7:1] = 7'b0;
+
+    // Todos los uio se dejan como entradas
+    assign uio_oe = 8'b0;
+
+    // Entradas no usadas para evitar warnings
     wire _unused = &{ui_in[7:1], uio_in, 1'b0};
 
-    tt_um_top_lfsr_register_8bit  dut (
-        .clk            (clk_i),
-        .reset_n        (rst_n_i),
-        .ena            (lfsr_enable_i),
-        .ui_in[0]       (reg_load_i),
-        .uo_out         (reg_data_o),
-        .uio_out[0]     (lfsr_done_o)
+    tt_um_top_lfsr_register_8bit dut (
+        .clk_i           (clk),
+        .reset_n_i       (rst_n),
+        .lfsr_enable_i   (lfsr_enable_i),
+        .reg_load_i      (reg_load_i),
+        .reg_data_o      (reg_data_o),
+        .lfsr_done_o     (lfsr_done_o)
     );
 
 endmodule
