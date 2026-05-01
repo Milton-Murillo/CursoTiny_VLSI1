54931494fd78e81401b734ca5550c599d30c9d2b
Milton-Murillo <141180204+Milton-Murillo@users.noreply.github.com>
2026-04-30 21:16:55 -0600

    Update output assignments and unused wire declaration

--- src/project.v
+++ src/project.v
@@ -17,11 +17,21 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  assign uo_out[7:1]  = 0;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out[7:1] = 0;
   assign uio_oe  = 0;
+  
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ui_in[7:1], uio_in, 1'b0};
+
+    top_lfsr_register_8bit dut (
+        .clk_i          (clk),
+        .reset_n_i      (rst_n),
+        .lfsr_enable_i  (ena),
+        .reg_load_i     (ui_in[0]),
+        .reg_data_o     (uo_out),
+        .lfsr_done_o    (uio_out[0])
+    );
 
 endmodule
